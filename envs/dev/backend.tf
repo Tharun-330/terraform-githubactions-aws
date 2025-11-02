adf069bc790188a8913e580c6489f70feb99e72f
Tharun-330 <tharunkumarthrn@gmail.com>
2025-11-02 14:05:35 +0530

    ignore dynamo db

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "state-github-bucket-karnati"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    #dynamodb_table = "terraform-lock"
+    #encrypt        = true
   }
 }
